078418e740ce0541c04a73b28f64f60071cf553b
Carl August Gjørsvik <carl.august.g@gmail.com>
2019-02-01 14:44:10 +0100

    GUI loads static floor grid

--- src/main/java/sky7/game/Game.java
+++ src/main/java/sky7/game/Game.java
@@ -1,9 +1,16 @@
 package sky7.game;
 
+import sky7.board.Board;
+import sky7.board.IBoard;
+
 public class Game implements IGame {
 
+    private IBoard board;
+    
     public Game() {
-        // TODO Auto-generated constructor stub
+        board = new Board();
+        
+        
     }
 
 }
