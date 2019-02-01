0f4e3575576e9b476971ad11072a6afa63f0a55d
Carl August Gjørsvik <carl.august.g@gmail.com>
2019-02-01 13:38:35 +0100

    GUI test and Main thread -> gui

---
+++ src/main/java/sky7/gui/GUI.java
@@ -0,0 +1,60 @@
+package sky7.gui;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GUI implements ApplicationListener {
+    
+    private SpriteBatch batch;
+    private BitmapFont font;
+    
+    public GUI() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.setColor(Color.RED);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        
+        batch.begin();
+        font.draw(batch, "Test", 200, 200);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int arg0, int arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
