f1665c5349042aaff7834fa1595186a5388aaf7c
HenrikHills <henrikgt_95@hotmail.com>
2019-02-04 19:21:43 +0100

    Edited FloorTile to make it easier to see where each grid is

--- src/main/java/sky7/board/cellContents/Inactive/FloorTile.java
+++ src/main/java/sky7/board/cellContents/Inactive/FloorTile.java
@@ -3,7 +3,7 @@ package sky7.board.cellContents.Inactive;
 import sky7.board.cellContents.IInactive;
 
 public class FloorTile implements IInactive {
-    String texture = "assets/Floor.jpg";
+    String texture = "assets/Floor.png";
 
     @Override
     public String getTexture() {
