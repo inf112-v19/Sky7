993e8ee964cf0caceb41c06b1cf92e1855e0068c
Fromsa Hera <fromsahera28@gmail.com>
2019-02-24 15:26:25 +0100

    Trying to create texture object in ICell classes.

--- src/main/java/sky7/board/cellContents/Inactive/FloorTile.java
+++ src/main/java/sky7/board/cellContents/Inactive/FloorTile.java
@@ -1,12 +1,18 @@
 package sky7.board.cellContents.Inactive;
 
+import com.badlogic.gdx.graphics.Texture;
 import sky7.board.ICell;
 import sky7.board.cellContents.IInactive;
 
 public class FloorTile implements IInactive {
 
     String textureRef = "floor";
+    Texture texture;
     int priority = 1;
+
+    public FloorTile(){
+        texture = new Texture("assets/Floor.png");;
+    }
 
     @Override
     public String getTexture() {
