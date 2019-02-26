2aa174e090e28c2350acd19bda4e7bacd399bfe3
Maren Gjermundrød <maren.andrea@hotmail.com>
2019-02-26 14:21:05 +0100

    Combined Player and Dock. Deleted Dock and IDock.

--- src/main/java/sky7/player/IPlayer.java
+++ src/main/java/sky7/player/IPlayer.java
@@ -5,4 +5,10 @@ public interface IPlayer {
     void applyDamage(int damage);
     
     void repair(int damage);
+
+    void lockCards(int n);
+
+    void unlockCards(int n);
+
+    void updateHealth(int x);
 }
