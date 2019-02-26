e6488e2f4599da0508f79f475717a3860bd8387b
Fromsa Hera <fromsahera28@gmail.com>
2019-02-26 11:02:36 +0100

    Reformating code

--- src/main/java/sky7/board/cellContents/Inactive/Wall.java
+++ src/main/java/sky7/board/cellContents/Inactive/Wall.java
@@ -1,4 +1,5 @@
 package sky7.board.cellContents.Inactive;
+
 import com.badlogic.gdx.graphics.Texture;
 import sky7.board.ICell;
 import sky7.board.cellContents.DIRECTION;
@@ -8,26 +9,32 @@ public class Wall implements IInactive {
     private DIRECTION direction;
     private Texture texture;
     private int priority = 3;
-    public Wall(DIRECTION direction){
+
+    public Wall(DIRECTION direction) {
         this.direction = direction;
         texture = new Texture(""); //TODO add wall images.
     }
+
     @Override
     public Texture getTexture() {
         return texture;
     }
+
     @Override
     public int drawPriority() {
         return priority;
     }
+
     @Override
     public int compareTo(ICell other) {
         return Integer.compare(this.drawPriority(), other.drawPriority());
     }
-    public DIRECTION getDirection(){
+
+    public DIRECTION getDirection() {
         return direction;
     }
-    public boolean canGoHere(DIRECTION incomingDir){
+
+    public boolean canGoHere(DIRECTION incomingDir) {
         return incomingDir.inverse(incomingDir) == direction;
     }
 }
