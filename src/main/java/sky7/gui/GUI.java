192bd8db2868355804976551d9832fc601e7c479
henrik <henrikgt_95@hotmail.com>
2019-03-06 14:50:21 +0100

    All cards clickable, error when using "setCard"

--- src/main/java/sky7/gui/GUI.java
+++ src/main/java/sky7/gui/GUI.java
@@ -66,7 +66,7 @@ public class GUI implements ApplicationListener {
 			textures.put("unmarkedCard", new Texture("assets/cards/EmptyCard.png"));
 
 			textureAtlas = new TextureAtlas("assets/cards/Cards.txt");
-			
+
 			hand = game.getHand();
 			addSprites();
 			initiateCards(hand);
@@ -153,6 +153,44 @@ public class GUI implements ApplicationListener {
 			batch.draw(textures.get("outline"), i * 128, 128);
 		}
 	}
+	
+	// check if user has chosen all 5 cards
+	// and put the chosen cards in the game-client, and lock the registry
+	public void playerCards() {
+		if (chosenCards[4] != null) {
+			cardsChoosen = true;
+
+			for (int i=0; i<chosenCards.length; i++) {
+				game.setCard(chosenCards[i], i);
+			}
+			game.lockRegistry();
+		}
+	}
+
+	// add sprites to a texturemap
+	public void addSprites() {
+		Array<AtlasRegion> regions = textureAtlas.getRegions();
+		for (AtlasRegion region : regions) {
+			Sprite sprite = textureAtlas.createSprite(region.name);
+			sprites.put(region.name, sprite);
+		}
+	}
+
+	// draw sprites
+	public void drawSprite(String name, float x, float y) {
+		Sprite sprite = sprites.get(name);
+		sprite.setPosition(x, y);
+		sprite.draw(batch);
+	}
+
+	// set the x position for the cards to spread them accross the map
+	private void initiateCards(ArrayList<ICard> hand) {
+		int cardX = 0;
+		for (ICard card : hand) {
+			card.setX(cardX);
+			cardX+=128;
+		}	
+	}
 
 	public void chooseCards() {
 		if (!cardsChoosen) {
@@ -178,60 +216,96 @@ public class GUI implements ApplicationListener {
 			if (Gdx.input.justTouched()) {
 				camera.unproject(clickPos.set(Gdx.input.getX(), Gdx.input.getY(), 0));
 				if(clickPos.x <= 128 && clickPos.y <= 128) {
-					chosenCards[pointer] = card0;
-					pointer++;
-					System.out.println(pointer + " card(s) choosen " + card0.GetSpriteRef());
-					card0.setX(yPos);
-					card0.setY(128);
-					yPos += 128;
+					if (card0.getY() != 128) {
+						chosenCards[pointer] = card0;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card0.GetSpriteRef());
+						card0.setX(yPos);
+						card0.setY(128);
+						yPos += 128;
+					}
 				}
 				if(clickPos.x <= 2*128 && clickPos.y <= 128 && clickPos.x > 128) {
-					chosenCards[pointer] = card1;
-					pointer++;
-					System.out.println(pointer + " card(s) choosen " + card1.GetSpriteRef());
-					card1.setX(yPos);
-					card1.setY(128);
-					yPos += 128;
+					if (card1.getY() != 128) {
+						chosenCards[pointer] = card1;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card1.GetSpriteRef());
+						card1.setX(yPos);
+						card1.setY(128);
+						yPos += 128;
+					}
+				}
+				if(clickPos.x <= 3*128 && clickPos.y <= 128 && clickPos.x > 128*2) {
+					if (card2.getY() != 128) {
+						chosenCards[pointer] = card1;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card2.GetSpriteRef());
+						card2.setX(yPos);
+						card2.setY(128);
+						yPos += 128;
+					}
+				}
+				if(clickPos.x <= 4*128 && clickPos.y <= 128 && clickPos.x > 128*3) {
+					if (card3.getY() != 128) {
+						chosenCards[pointer] = card3;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card3.GetSpriteRef());
+						card3.setX(yPos);
+						card3.setY(128);
+						yPos += 128;
+					}
+				}
+				if(clickPos.x <= 5*128 && clickPos.y <= 128 && clickPos.x > 128*4) {
+					if (card4.getY() != 128) {
+						chosenCards[pointer] = card4;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card4.GetSpriteRef());
+						card4.setX(yPos);
+						card4.setY(128);
+						yPos += 128;
+					}
+				}
+				if(clickPos.x <= 6*128 && clickPos.y <= 128 && clickPos.x > 128*5) {
+					if (card5.getY() != 128) {
+						chosenCards[pointer] = card5;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card5.GetSpriteRef());
+						card5.setX(yPos);
+						card5.setY(128);
+						yPos += 128;
+					}
+				}
+				if(clickPos.x <= 7*128 && clickPos.y <= 128 && clickPos.x > 128*6) {
+					if (card6.getY() != 128) {
+						chosenCards[pointer] = card6;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card6.GetSpriteRef());
+						card6.setX(yPos);
+						card6.setY(128);
+						yPos += 128;
+					}
+				}
+				if(clickPos.x <= 8*128 && clickPos.y <= 128 && clickPos.x > 128*7) {
+					if (card7.getY() != 128) {
+						chosenCards[pointer] = card7;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card7.GetSpriteRef());
+						card7.setX(yPos);
+						card7.setY(128);
+						yPos += 128;
+					}
+				}
+				if(clickPos.x <= 9*128 && clickPos.y <= 128 && clickPos.x > 128*8) {
+					if (card8.getY() != 128) {
+						chosenCards[pointer] = card8;
+						pointer++;
+						System.out.println(pointer + " card(s) choosen " + card8.GetSpriteRef());
+						card8.setX(yPos);
+						card8.setY(128);
+						yPos += 128;
+					}
 				}
 			}
 		}
-	}
-
-	// check if user has chosen all 5 cards
-	// and put the chosen cards in the game-client, and lock the registry
-	public void playerCards() {
-		if (chosenCards[4] != null) {
-			cardsChoosen = true;
-			
-			for (int i=0; i<chosenCards.length; i++) {
-				game.setCard(chosenCards[i], i);
-			}
-			game.lockRegistry();
-		}
-	}
-
-	// add sprites to a texturemap
-	public void addSprites() {
-		Array<AtlasRegion> regions = textureAtlas.getRegions();
-		for (AtlasRegion region : regions) {
-			Sprite sprite = textureAtlas.createSprite(region.name);
-			sprites.put(region.name, sprite);
-		}
-	}
-
-	// draw sprites
-	public void drawSprite(String name, float x, float y) {
-		Sprite sprite = sprites.get(name);
-		sprite.setPosition(x, y);
-		sprite.draw(batch);
-	}
-	
-// set the x position for the cards to spread them accross the map
-	private void initiateCards(ArrayList<ICard> hand) {
-		int cardX = 0;
-		for (ICard card : hand) {
-			card.setX(cardX);
-			cardX+=128;
-		}	
 	}
 }
