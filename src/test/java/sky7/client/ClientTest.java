f6475f61032751b125cc8d6597789fef0d054c2e
Carl August Gjørsvik <carl.august.g@gmail.com>
2019-03-15 15:36:43 +0100

    Tests for Client

--- src/test/java/sky7/client/ClientTest.java
+++ src/test/java/sky7/client/ClientTest.java
@@ -2,6 +2,7 @@ package sky7.client;
 
 import static org.junit.Assert.*;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import sky7.card.ICard;
+import sky7.card.ProgramCard;
 import sky7.card.ProgramDeck;
 import sky7.game.Client;
 import sky7.game.IClient;
@@ -36,12 +38,21 @@ public class ClientTest {
         
         
         for (int i=0; i<N_CLIENTS ; i++) {
-            clients.get(i).connect(host, i);
+            clients.get(i).connect(host, i, "assets/Boards/emptyBoard.json");
             
             assertEquals(clients.get(i).getPlayer().getPlayerNumber(), i);
         }
         
-        
+        setupBoard();
+    }
+
+    public static void setupBoard() {
+        try {
+            clients.get(0).generateBoard();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
     
     @Test
@@ -51,6 +62,14 @@ public class ClientTest {
         clients.get(0).chooseCards(given);
         
         assertEquals(given, clients.get(0).getHand());
+        
+    }
+    
+    @Test
+    public void testRobot() {
+        // at the moment, client places robot at 5,5 in generateBoard() method, otherwise we would have to place it now
+        clients.get(0).activateCard(0, new ProgramCard(0,2,0));
+        
         
     }
 }
