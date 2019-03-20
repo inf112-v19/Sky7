0874d6ab63508000410b35218c423d9357ffc319
Carl August Gjørsvik <Carl.Gjorsvik@student.uib.no>
2019-03-20 11:24:48 +0100

    Added TestSuite to combine and run tests more easily

---
+++ src/test/java/sky7/TestSuite.java
@@ -0,0 +1,23 @@
+package sky7;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import sky7.BoardTests.BoardGeneratorTest;
+import sky7.BoardTests.BoardTests;
+import sky7.client.ClientTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    BoardGeneratorTest.class,
+    BoardTests.class,
+    ClientTest.class,
+    FlagTest.class,
+    ICellTest.class,
+    ProgramDeckTest.class
+})
+
+public class TestSuite {
+
+}
