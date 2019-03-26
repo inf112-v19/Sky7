967ede413e53856720944a976aa15a557c3b0eb5
Fromsa Hera <fromsahera28@gmail.com>
2019-03-26 10:40:09 +0100

    deleted useless method

--- src/main/java/sky7/card/ProgramDeck.java
+++ src/main/java/sky7/card/ProgramDeck.java
@@ -60,10 +60,6 @@ public class ProgramDeck implements IDeck {
         Collections.shuffle(availableCards);
     }
 
-    @Override
-    public int getSize() {
-        return availableCards.size();
-    }
 
 
     public ProgramCard[] getProgramCardsForTesting(){
