7a57f06adeb77b1af9dff5fba66a69c01fcb26c9
Maren <maren.andrea@hotmail.com>
2019-03-26 11:58:17 +0100

    Updated getTexture-method in Laser.

--- src/main/java/sky7/board/cellContents/Active/Laser.java
+++ src/main/java/sky7/board/cellContents/Active/Laser.java
@@ -3,15 +3,17 @@ package sky7.board.cellContents.Active;
 import com.badlogic.gdx.graphics.Texture;
 import sky7.board.ICell;
 import sky7.board.cellContents.IActive;
-import sky7.board.cellContents.IInactive;
+import sky7.board.cellContents.DIRECTION;
 
 public class Laser implements IActive {
     private final boolean start;
-    private final int direction;
+    private DIRECTION direction;
+    //private final int direction; //
     private final int numberOfLasers;
+    private Texture texture;
     private final static int PRIORITY = 3;
 
-    public Laser(boolean start, int direction, int numberOfLasers) {
+    public Laser(boolean start, DIRECTION direction, int numberOfLasers) {
 
         this.start = start;
         this.direction = direction;
@@ -20,7 +22,55 @@ public class Laser implements IActive {
 
     @Override
     public Texture getTexture() {
-        return null;
+        if(texture == null){
+            if(start){
+                switch (direction) {
+                    case NORTH:
+                        if (numberOfLasers == 1) {
+                            texture = new Texture("assets/laser/LaserStartU.png");
+                        } else {
+                            texture = new Texture("assets/laser/LaserDStartU.png");
+                        }break;
+                    case SOUTH:
+                        if (numberOfLasers == 1) {
+                            texture = new Texture("assets/laser/LaserStartD.png");
+                        } else {
+                            texture = new Texture("assets/laser/LaserDStartD.png");
+                        }break;
+                    case WEST:
+                        if (numberOfLasers == 1) {
+                            texture = new Texture("assets/laser/LaserStartR.png");
+                        } else {
+                            texture = new Texture("assets/laser/LaserDStartR.png");
+                        }break;
+                    case EAST:
+                        if (numberOfLasers == 1) {
+                            texture = new Texture("assets/laser/LaserStartL.png");
+                        } else {
+                            texture = new Texture("assets/laser/LaserDStartL.png");
+                        }break;
+                }
+
+
+            }else {
+                if( direction == DIRECTION.SOUTH || direction == DIRECTION.NORTH){
+                    if(numberOfLasers == 1){
+                        texture = new Texture("assets/laser/LaserVert.png");
+                    }else {
+                        texture = new Texture("assets/laser/LaserDVert.png");
+                    }
+                }else {
+                    if(numberOfLasers == 1){
+                        texture = new Texture("assets/laser/LaserHor.png");
+                    }else {
+                        texture = new Texture("assets/laser/LaserDHor.png");
+                    }
+                }
+
+            }
+        }
+
+        return texture;
     }
 
     @Override
