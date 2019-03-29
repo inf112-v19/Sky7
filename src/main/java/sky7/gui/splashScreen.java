6dba25eb4529cc819c3c63aeb1a12c50c42d7a1e
HenrikHills <hto017@student.uib.no>
2019-03-29 14:59:26 +0100

    Added clickable button to splashscreen; Need to change texture and make it nicer

--- src/main/java/sky7/gui/splashScreen.java
+++ src/main/java/sky7/gui/splashScreen.java
@@ -1,14 +1,13 @@
 package sky7.gui;
 
 import java.util.HashMap;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -21,40 +20,54 @@ public class splashScreen implements ApplicationListener {
 	private OrthographicCamera camera;
 	private Vector3 clickPos = new Vector3();
 	private HashMap<String, Texture> textures;
-	private int scaler = 1;
 	private int windowWidth, windowHeight;
-	private BitmapFont font;
 	private Stage stage;
+	private Sprite spr;
 
 	public splashScreen() {
 		textures = new HashMap<>();
 	}
-	public class MyActor extends Actor {
-		
-		@Override
+
+	public class Background extends Actor {
 		public void draw(Batch batch, float alpha){
 			batch.draw(textures.get("splashScreen"),0,0, Gdx.graphics.getWidth(), Gdx.graphics.getWidth());
+		}
+	}
+
+	public class Button extends Actor {
+		public void draw(Batch batch, float alpha) {
+			spr.draw(batch, alpha);
+			if (isClicked(spr)) {
+				//TODO: initiate GUI/ the game
+				System.out.println("You pressed me!");
+			}
 		}
 	}
 
 
 	@Override
 	public void create() {
-        windowWidth = Gdx.graphics.getWidth();
-        windowHeight = Gdx.graphics.getWidth();
+		windowWidth = Gdx.graphics.getWidth();
+		windowHeight = Gdx.graphics.getWidth();
 
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.getData().setScale(5, 5);
-        camera = new OrthographicCamera();
-        viewport = new ExtendViewport(windowWidth * scaler, windowHeight * scaler, camera);
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		viewport = new ExtendViewport(windowWidth, windowHeight, camera);
 
-        textures.put("splashScreen", new Texture("assets/menu/splashScreen.png"));
+		textures.put("splashScreen", new Texture("assets/menu/splashScreen.png"));
+		textures.put("button", new Texture("assets/dock/Confirm.png"));
 
-        stage = new Stage(viewport);
-        MyActor myActor = new MyActor();
-        stage.addActor(myActor);
-        Gdx.input.setInputProcessor(stage);
+		spr = new Sprite(textures.get("button"));
+		spr.setX(6*128);
+		spr.setY(6*128);
+		
+		stage = new Stage(viewport);
+		Background myActor = new Background();
+		Button myButton = new Button();
+
+		stage.addActor(myActor);
+		stage.addActor(myButton);
+		Gdx.input.setInputProcessor(stage);
 	}
 
 	@Override
@@ -65,17 +78,10 @@ public class splashScreen implements ApplicationListener {
 
 	@Override
 	public void render() {
-		float delta = Gdx.graphics.getDeltaTime();
 		Gdx.gl.glClearColor(0, 0, 0, 1); // Background Color
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clears every image from previous iteration of render.
 		batch.setProjectionMatrix(camera.combined);
 
-//		batch.begin();
-//		batch.draw(textures.get("splashScreen"), 0, 0, windowWidth*scaler, windowHeight*scaler);
-//		font.draw(batch, "testing shit", 7*scaler, 8*scaler);
-//		batch.end();
-
-		stage.act(delta);
 		stage.draw();
 	}
 
@@ -95,5 +101,17 @@ public class splashScreen implements ApplicationListener {
 	public void dispose() {	
 		stage.dispose();
 	}
-
+	
+	public boolean isClicked(Sprite sprite) {
+		if (Gdx.input.justTouched()){
+			camera.unproject(clickPos.set(Gdx.input.getX(), Gdx.input.getY(), 0));
+			if (clickPos.x > sprite.getX() && clickPos.x < sprite.getX() + sprite.getWidth()) {
+				if (clickPos.y > sprite.getY() && clickPos.y < sprite.getY() + sprite.getHeight()) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
 }
