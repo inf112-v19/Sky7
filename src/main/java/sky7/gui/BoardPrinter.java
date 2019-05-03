d8b6ad67255819258cf3e8e5e40ee4bc1ae3c635
Brigt Arve Toppe Håvardstun <Brigt_arve@outlook.com>
2019-05-03 12:15:50 +0200

    removed copy from tile to list, tested wanted to remove concurent bug

--- src/main/java/sky7/gui/BoardPrinter.java
+++ src/main/java/sky7/gui/BoardPrinter.java
@@ -6,8 +6,6 @@ import sky7.Client.IClient;
 import sky7.board.ICell;
 import sky7.board.cellContents.robots.RobotTile;
 
-import java.util.ArrayList;
-
 public class BoardPrinter {
 	int width;
 	int height;
@@ -24,9 +22,8 @@ public class BoardPrinter {
 	// draw the gameboard as a grid of width*height, each square at 128*128 pixels
 	public void showBoard(IClient game) {
 		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				ArrayList<ICell> list = new ArrayList<>(game.gameBoard().getTileTexture(i,j));
-				for (ICell cell : list) {
+			for (int j = 0; j < width; j++) {g
+				for (ICell cell : game.gameBoard().getTileTexture(i, j)) {
 					if (cell instanceof RobotTile) {
 						int rotation = findRotation((RobotTile)cell);
 						batch.draw(new TextureRegion(cell.getTexture()), (i+2)*scaler, (j+2)*scaler, scaler/2, scaler/2, scaler, scaler, 1, 1, rotation);
